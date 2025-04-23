8c4161d05fc5238889b4b18af5da7c7cc71680ae
EsneiderCdD <ecd.97@hotmail.com>
2025-04-23 16:25:01 -0500

    deploy

--- src/componentes/ModalHome/helpers.js
+++ src/componentes/ModalHome/helpers.js
@@ -1,4 +1,4 @@
-import linksDePago from " ./linksDePago.js";
+import linksDePago from "./linksDePago.js";
 export function obtenerLinkDePago(sesiones, pais) {
   const clave = `${sesiones}-${pais}`;
   return linksDePago[clave];
