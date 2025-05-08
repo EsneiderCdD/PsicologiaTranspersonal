2eabb4225fce127fec2a0effc774097ceb65c253
EsneiderCdD <ecd.97@hotmail.com>
2025-05-07 22:02:42 -0500

    90%

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -120,7 +120,7 @@ export default function Home() {
             className={estilos.botonSecundario}
             onClick={() => navigate("/about")}
           >
-            Sobre mí
+            Sobre la terapeuta
           </button>
         </motion.div>
       </motion.div>
