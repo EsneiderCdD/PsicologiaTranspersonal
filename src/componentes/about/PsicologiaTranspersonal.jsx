6fcef07e3fdc888ff363e0b8324eb06fe7e725df
EsneiderCdD <ecd.97@hotmail.com>
2025-06-01 14:06:45 -0500

    responsive

--- src/componentes/about/PsicologiaTranspersonal.jsx
+++ src/componentes/about/PsicologiaTranspersonal.jsx
@@ -4,8 +4,25 @@ import mariposa from "../../assets/imagenes/mariposa.jpg";
 import ft0 from "../../assets/imagenes/ft0.jpeg"; 
 import Encabezado from "../global/Encabezado";
 import { FaLeaf, FaHeart, FaHandsHelping, FaStar } from 'react-icons/fa';
+import useMediaQuery from "../Hooks/useMediaQuery";
 
 export default function PsicologiaTranspersonal() {
+  const isMobile = useMediaQuery("(max-width: 750px)");
+  const isVerySmall = useMediaQuery("(max-width: 550px)");
+  const parrafoIntermedio = (
+  <p>
+    Nace y se desarrolla desde la coherencia interna, desde el deseo genuino de acompañar a otros en su despertar, en su transformación, en su reconexión con su verdad más esencial.
+  </p>
+);
+
+  const parrafoFinal = (
+    <p className={estilos.parrafoFinal}>
+      Con el tiempo, la pasión se tradujo en formación, experiencia y una comunidad que crece desde la autenticidad. Cada palabra, sesión y creación digital refleja ese compromiso con el alma y la sanación.
+      Desde ahí, nace también el impulso de crear espacios colectivos, como los círculos de mujeres, que dan continuidad a este viaje compartido.
+    </p>
+
+    
+  );
   return (
     <div>
       <Encabezado
@@ -22,28 +39,33 @@ export default function PsicologiaTranspersonal() {
             
             <p>Psicotranspersonal nace de mi pasión por la profundidad, por el misterio, por los escritos que nutren la visión y la mirada, por mi deseo de compartir lo que habita dentro de mi corazón, se gesta hace 6 años en un anhelo de darle voz a procesos y vivencias que giran en torno a la sanación, la transformación humana y la evolución del ser </p>
 
+             {isVerySmall && <div className={estilos.parrafoExtra}>{parrafoIntermedio}</div>}
+
           </div>
 
           <div className={estilos.ts2}>
               <div className={estilos.cn}>
                 
                   <div className={estilos.texto}>
-                    <p>Nace y se desarrolla desde la coherencia interna, desde el deseo genuino de acompañar a otros en su despertar, en su transformación, en su reconexión con su verdad más esencial. </p>
+                    {!isVerySmall && parrafoIntermedio}
 
                     <p>Lo que comenzó como una búsqueda personal se transformó en un proyecto con propósito. Psicotranspersonal tomó forma como un espacio profesional y consciente, nacido del deseo genuino de acompañar procesos de transformación, de crear vínculos conscientes, de sostener espacios seguros, sagrados y humanos donde las personas pueden mirar adentro, sanar heridas, recordar quiénes son y conectar con su alma.
                       </p>
 
-                    <p>Con el tiempo, la pasión se tradujo en formación, experiencia y una comunidad que crece desde la autenticidad. Cada palabra, sesión y creación digital refleja ese compromiso con el alma y la sanación.
-                    Desde ahí, nace también el impulso de crear espacios colectivos,como los círculos de mujeres, que dan continuidad a este viaje compartido.
+                     {/* SOLO se muestra si NO es móvil */}
+                {!isMobile && parrafoFinal}
 
-</p>
-                 
+                   
                   </div>
 
 
                   <img src={mariposa} alt="SUSANASIMBOLO" />
 
               </div>  
+               {/* SOLO se muestra si SÍ es móvil */}
+            {isMobile && (
+              <div className={estilos.parrafoFinal}>{parrafoFinal}</div>
+            )}
             
           </div>
 
