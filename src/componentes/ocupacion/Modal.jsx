5a94674e0ba9f7222825411929c2532f84669507
EsneiderCdD <ecd.97@hotmail.com>
2025-07-03 22:04:33 -0500

    Precio Normalizado

--- src/componentes/ocupacion/Modal.jsx
+++ src/componentes/ocupacion/Modal.jsx
@@ -17,7 +17,7 @@ const Modal = ({
 
   const externalLinks = {
     "1 Sesión": "https://biz.payulatam.com/L0f9bc36813670A",
-    "4 Sesiones": "https://biz.payulatam.com/L0f9bc376663FC4",
+    "4 Sesiones": "https://biz.payulatam.com/L0f9bc32484A754",
     "6 Sesiones": "https://biz.payulatam.com/L0f9bc3B6BDDFD6",
   };
 
