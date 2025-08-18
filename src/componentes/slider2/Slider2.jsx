447cd8767c3e1f0324d413c129f3d72f01bb4b44
EsneiderCdD <ecd.97@hotmail.com>
2025-08-18 09:18:49 -0500

    fix : imagenes

--- src/componentes/slider2/Slider2.jsx
+++ src/componentes/slider2/Slider2.jsx
@@ -17,13 +17,13 @@ import styles from './Slider2.module.css';
 import ft0 from '../../assets/imagenes/ft0.jpeg';
 import ft1 from '../../assets/imagenes/ft1.jpeg';
 import ft2 from '../../assets/imagenes/ft2.jpeg';
-import ft3 from '../../assets/imagenes/ft3.jpeg';
+import Img3 from '../../assets/imagenes/Img3.webp';
 import ft4 from '../../assets/imagenes/ft4.jpeg';
 import ft5 from '../../assets/imagenes/ft5.jpeg';
 import ft6 from '../../assets/imagenes/ft6.jpeg';
 import ft7 from '../../assets/imagenes/ft7.jpeg';
 import { useNavigate } from 'react-router-dom';
-const images = [ft0, ft3, ft5, ft1, ft7, ft4, ft2, ft6];
+const images = [ft0, Img3, ft5, ft1, ft7, ft4, ft2, ft6];
 
 const Slider2 = () => {
   const location = useLocation();
